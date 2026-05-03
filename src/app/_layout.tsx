739dbd369e95d324eeb183c4080997f3d5e34db1
Arooran Thanabalasingam <13069024+bigaru@users.noreply.github.com>
2026-05-03 19:08:07 +0200

    chore: add tamagui

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,16 +1,20 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import React from 'react';
-import { useColorScheme } from 'react-native';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
+import React from 'react'
+import { useColorScheme } from 'react-native'
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
+import { AnimatedSplashOverlay } from '@/components/animated-icon'
+import AppTabs from '@/components/app-tabs'
+import { TamaguiProvider } from '@tamagui/core'
+import { config } from '../../tamagui.config'
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
-  );
+	const colorScheme = useColorScheme()
+	return (
+		<TamaguiProvider config={config} defaultTheme={colorScheme!}>
+			<ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+				<AnimatedSplashOverlay />
+				<AppTabs />
+			</ThemeProvider>
+		</TamaguiProvider>
+	)
 }
