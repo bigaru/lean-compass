57b464b33aff305f67b32da2e9ff9b83c138f222
Arooran Thanabalasingam <13069024+bigaru@users.noreply.github.com>
2026-05-06 18:31:25 +0200

    refactor: move logic to store

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,44 +1,11 @@
 import { useStore } from '@/store'
 import { CornerDownLeft } from '@tamagui/lucide-icons-2'
-import { useMemo } from 'react'
 import { Button, Card, Text, TextArea, XStack, YStack } from 'tamagui'
-import { useDebounce } from 'use-debounce'
-
-function matchNumbers(input: string, regex: RegExp): number | undefined {
-	const found = [...input.matchAll(regex)].map((i) => i[1])
-	return found.length === 1 ? Number(found[0]) : undefined
-}
-
-function match(input: string, regex: RegExp, acceptMany: boolean = false): string | undefined {
-	const found = [...input.matchAll(regex)].map((i) => i[1])
-	return acceptMany || found.length === 1 ? found[0] : undefined
-}
 
 const LINE_HEIGHT = 30
 
-const word = /\b(?!\d)(\w+)\b/g
-const weight = /\b(\d+(?:\.\d+)?)g\b/g
-const calories = /\b(\d+(?:\.\d+)?)cal\b/g
-const fat = /\b(\d+(?:\.\d+)?)f\b/g
-const carb = /\b(\d+(?:\.\d+)?)c\b/g
-const protein = /\b(\d+(?:\.\d+)?)p\b/g
-const other = /\b(\d+(?:\.\d+)?(?!(?:g|f|c|p|cal)\b))[a-zA-Z]*\b/g
-const allRegexes: Record<string, RegExp> = { weight, calories, fat, carb, protein }
-
 export default function HomeScreen() {
-	const { mainInput, setMainInput, foods, addFood } = useStore((state) => state)
-	const [debouncedInput] = useDebounce(mainInput, 500)
-
-	const matches = useMemo(() => {
-		const entries = Object.keys(allRegexes).map((k) => [k, matchNumbers(debouncedInput, allRegexes[k])])
-		entries.push(['word', match(debouncedInput, word)])
-		entries.push(['other', match(debouncedInput, other, true)])
-		return Object.fromEntries(entries)
-	}, [debouncedInput])
-
-	const hasTrio = matches.fat != null || matches.carb != null || matches.protein != null
-	const hasCaloricValue = (matches.calories != null) !== hasTrio
-	const isValid = !mainInput.trim() || (matches.weight != null && hasCaloricValue && !matches.other)
+	const { mainInput, setMainInput, isInputValid, foods, addFood } = useStore((state) => state)
 
 	return (
 		<YStack height={'100%'}>
@@ -56,13 +23,13 @@ export default function HomeScreen() {
 				<TextArea
 					grow={1}
 					height={LINE_HEIGHT * 3}
-					focusStyle={{ borderColor: isValid ? 'gray' : 'red' }}
+					focusStyle={{ borderColor: isInputValid ? 'gray' : 'red' }}
 					placeholder="..."
 					value={mainInput}
 					textAlignVertical="top"
 					onChangeText={setMainInput}
 				/>
-				<Button disabled={!isValid} theme={isValid ? 'blue_accent' : 'gray'} onPress={() => addFood(matches)} height={'100%'} icon={CornerDownLeft}></Button>
+				<Button disabled={!isInputValid} theme={isInputValid ? 'blue_accent' : 'gray'} onPress={addFood} height={'100%'} icon={CornerDownLeft}></Button>
 			</XStack>
 		</YStack>
 	)
--- src/store.ts
+++ src/store.ts
@@ -1,3 +1,4 @@
+import debounce from 'lodash.debounce'
 import { create } from 'zustand'
 
 interface Food {
@@ -10,44 +11,91 @@ interface Food {
 	totalCalories: number
 }
 
-interface State {
-	mainInput: string
-	foods: Food[]
-	setMainInput: (text: string) => void
-	addFood: (matches: Record<string, any>) => void
-}
-
 //per gram
 const CAL_OF_FAT = 9
 const CAL_OF_CARB = 4
 const CAL_OF_PROTEIN = 4
 
-const useStore = create<State>((set) => ({
-	mainInput: '',
-	foods: [],
-	setMainInput: (text) => set((state) => ({ ...state, mainInput: text })),
-	addFood: (matches: Record<string, any>) =>
-		set((state) => {
-			const { other, ...rest } = matches
+function matchNumbers(input: string, regex: RegExp): number | undefined {
+	const found = [...input.matchAll(regex)].map((i) => i[1])
+	return found.length === 1 ? Number(found[0]) : undefined
+}
 
-			const fat = rest.fat ?? 0
-			const carb = rest.carb ?? 0
-			const protein = rest.protein ?? 0
+function match(input: string, regex: RegExp, acceptMany: boolean = false): string | undefined {
+	const found = [...input.matchAll(regex)].map((i) => i[1])
+	return acceptMany || found.length === 1 ? found[0] : undefined
+}
 
-			const calPer100 = rest.calories ? rest.calories : CAL_OF_FAT * fat + CAL_OF_CARB * carb + CAL_OF_PROTEIN * protein
+const word = /\b(?!\d)(\w+)\b/g
+const weight = /\b(\d+(?:\.\d+)?)g\b/g
+const calories = /\b(\d+(?:\.\d+)?)cal\b/g
+const fat = /\b(\d+(?:\.\d+)?)f\b/g
+const carb = /\b(\d+(?:\.\d+)?)c\b/g
+const protein = /\b(\d+(?:\.\d+)?)p\b/g
+const other = /\b(\d+(?:\.\d+)?(?!(?:g|f|c|p|cal)\b))[a-zA-Z]*\b/g
+const allRegexes: Record<string, RegExp> = { weight, calories, fat, carb, protein }
 
-			const newFood = {
-				...rest,
-				name: rest.word ?? '',
-				fat,
-				carb,
-				protein,
-				calories: rest.calories ?? 0,
-				totalCalories: calPer100 * (rest.weight / 100),
-			} as Food
+function computeMatches(input: string) {
+	const entries = Object.keys(allRegexes).map((k) => [k, matchNumbers(input, allRegexes[k])])
+	entries.push(['word', match(input, word)])
+	entries.push(['other', match(input, other, true)])
+	return Object.fromEntries(entries)
+}
 
-			return { ...state, mainInput: '', foods: [...state.foods, newFood] }
-		}),
-}))
+function validateMatches(matches: Record<string, any>) {
+	const hasTrio = matches.fat != null || matches.carb != null || matches.protein != null
+	const hasCaloricValue = (matches.calories != null) !== hasTrio
+	return matches.weight != null && hasCaloricValue && !matches.other
+}
+
+interface State {
+	mainInput: string
+	matches: Record<string, any>
+	isInputValid: boolean
+	foods: Food[]
+	setMainInput: (text: string) => void
+	addFood: () => void
+}
+
+const useStore = create<State>((set) => {
+	const debounceInput = debounce((input: string) => {
+		const matches = computeMatches(input)
+		set((state) => ({ ...state, matches, isInputValid: !state.mainInput.trim() || validateMatches(matches) }))
+	}, 500)
+
+	return {
+		mainInput: '',
+		matches: {},
+		foods: [],
+		isInputValid: true,
+		setMainInput: (text) =>
+			set((state) => {
+				debounceInput(text)
+				return { ...state, mainInput: text }
+			}),
+		addFood: () =>
+			set((state) => {
+				const { other, ...rest } = state.matches
+
+				const fat = rest.fat ?? 0
+				const carb = rest.carb ?? 0
+				const protein = rest.protein ?? 0
+
+				const calPer100 = rest.calories ? rest.calories : CAL_OF_FAT * fat + CAL_OF_CARB * carb + CAL_OF_PROTEIN * protein
+
+				const newFood = {
+					...rest,
+					name: rest.word ?? '',
+					fat,
+					carb,
+					protein,
+					calories: rest.calories ?? 0,
+					totalCalories: calPer100 * (rest.weight / 100),
+				} as Food
+
+				return { ...state, mainInput: '', foods: [...state.foods, newFood] }
+			}),
+	}
+})
 
 export { useStore }
