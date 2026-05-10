b870e39548403132528a370be7e631876467bf09
Arooran Thanabalasingam <13069024+bigaru@users.noreply.github.com>
2026-05-10 12:36:39 +0200

    feat: display empty in stats

--- src/app/(tabs)/stats.tsx
+++ src/app/(tabs)/stats.tsx
@@ -3,7 +3,7 @@ import { deviceLocale } from '@/constants/locale'
 import { useStore } from '@/store'
 import { addDaysToDate } from '@/utils'
 import { Text as SkiaText, useFont } from '@shopify/react-native-skia'
-import { ChevronLeft, ChevronRight } from '@tamagui/lucide-icons-2'
+import { ChevronLeft, ChevronRight, CircleSlash2 } from '@tamagui/lucide-icons-2'
 import { Stack } from 'expo-router'
 import { useEffect, useRef } from 'react'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
@@ -74,15 +74,22 @@ const fatColor = '#E01A4F'
 const carbColor = '#F15946'
 const proteinColor = '#F9C22E'
 
+const range = (start: number, end: number) => Array.from({ length: end - start }, (_, i) => start + i)
+
 function ChartPage({ index }: InfinitePagerPageProps) {
 	const { loadLastSeven } = useStore((state) => state)
 	useEffect(() => {
 		loadLastSeven(index)
 	}, [])
 
+	const tickValues = range(index * 7 - 6, index * 7 + 1).map((k) => addDaysToDate(k).valueOf())
 	const chartData = useStore((state) => state.chartRecord[index]) ?? []
 	const font = useFont(inter, 12)
 	const maxY = Math.floor(Math.max(...chartData.map((d) => d.cal)) * 1.1)
+
+	if (chartData.length === 0) {
+		return <EmptyState />
+	}
 
 	return (
 		<YStack p="$3" height="100%">
@@ -90,7 +97,7 @@ function ChartPage({ index }: InfinitePagerPageProps) {
 				data={chartData}
 				padding={{ top: 30 }}
 				yAxis={[{ font: font, formatYLabel: formatNumber }]}
-				xAxis={{ font: font, tickValues: chartData.map((d) => d.createdAt), formatXLabel: (val) => formatLabelX(val) }}
+				xAxis={{ font: font, tickValues, tickCount: tickValues.length, formatXLabel: (val) => formatLabelX(val) }}
 				xKey="createdAt"
 				yKeys={['fat', 'carb', 'protein']}
 				domainPadding={{ left: 50, right: 50 }}
@@ -102,7 +109,7 @@ function ChartPage({ index }: InfinitePagerPageProps) {
 				)}
 			>
 				{({ points, chartBounds }) => (
-					<StackedBar points={[points.fat, points.carb, points.protein]} chartBounds={chartBounds} colors={[fatColor, carbColor, proteinColor]} />
+					<StackedBar points={[points.fat, points.carb, points.protein]} chartBounds={chartBounds} barWidth={40} colors={[fatColor, carbColor, proteinColor]} />
 				)}
 			</CartesianChart>
 			<XStack gap="$5" justify="center" mt="$4">
@@ -111,7 +118,7 @@ function ChartPage({ index }: InfinitePagerPageProps) {
 					[carbColor, 'Carb'],
 					[proteinColor, 'Protein'],
 				].map(([color, text]) => (
-					<XStack items="center" gap="$2">
+					<XStack items="center" gap="$2" key={text}>
 						<View
 							bg={color as any}
 							borderTopRightRadius="$5"
@@ -124,6 +131,19 @@ function ChartPage({ index }: InfinitePagerPageProps) {
 						<SizableText size="$5">{text}</SizableText>
 					</XStack>
 				))}
+			</XStack>
+		</YStack>
+	)
+}
+
+function EmptyState() {
+	return (
+		<YStack justify="center" items="center" height="100%">
+			<XStack gap="$2">
+				<SizableText mb="$4" size="$8" fontWeight={'bold'}>
+					No Data
+				</SizableText>
+				<CircleSlash2 size="$3" />
 			</XStack>
 		</YStack>
 	)
